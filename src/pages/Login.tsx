6fd938d9a7849858c3e1b6506b99383c0b31d3d2
tommaso caputi <tommasocaputi85@gmail.com>
2023-03-11 18:49:21 +0100

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,23 @@
-import { IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonContent, IonInput, IonItem, IonLabel, useIonAlert, } from '@ionic/react';
+import {
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonContent,
+  IonInput,
+  IonItem,
+  IonLabel,
+  useIonAlert,
+} from "@ionic/react";
 import { createBrowserHistory } from "history";
-import { useState } from 'react';
+import { useState } from "react";
 
 const history = createBrowserHistory({ forceRefresh: true });
 
 const Login: React.FC = () => {
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [presentAlert] = useIonAlert();
 
   const login = () => {
@@ -15,60 +26,87 @@ const Login: React.FC = () => {
     fetch("https://cryptokeeper.altervista.org/APP/webhook.php", {
       method: "POST",
       body: JSON.stringify({
-        "action": "login",
-        "email": email,
-        "password": password,
-      })
-    })
-      .then(response => {
-        response.text()
-          .then(response => {             
-            let a = response.split('.')
-            if (a[1]==='True') {
-              presentAlert({
-                header: 'Success',
-                message: "Logged successfully",
-                buttons: ['OK'],
-              })
-              history.push('/Menu', { email: email ,mnemonic:a[0]},);
-            } else {
-              presentAlert({
-                header: 'Failed',
-                message: "Email or password are incorrect",
-                buttons: ['OK'],
-              })
-            }
+        action: "login",
+        email: email,
+        password: password,
+      }),
+    }).then((response) => {
+      response.text().then((response) => {
+        let a = response.split(".");
+        if (a[1] === "True") {
+          presentAlert({
+            header: "Success",
+            message: "Logged successfully",
+            buttons: ["OK"],
           });
-      })
-  }
-
+          history.push("/Menu", { email: email, mnemonic: a[0] });
+        } else {
+          presentAlert({
+            header: "Failed",
+            message: "Email or password are incorrect",
+            buttons: ["OK"],
+          });
+        }
+      });
+    });
+  };
 
   return (
     <IonPage>
-      <div style={{ padding: '20px', height: '100%', width: '100%', display: 'flex', flexDirection: 'column', backgroundColor: 'white', color: 'black' }}>
-        <div style={{ height: '8%' }}></div>
+      <div
+        style={{
+          padding: "20px",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "white",
+          color: "black",
+        }}
+      >
+        <div style={{ height: "8%" }}></div>
         <div>
-          <h1 style={{ fontSize: 40 }}>Log in<br></br>to continue</h1>
+          <h1 style={{ fontSize: 40 }}>
+            Log in<br></br>to continue
+          </h1>
         </div>
-        <div style={{ paddingTop: '40px', }}>
+        <div style={{ paddingTop: "40px" }}>
           <IonLabel position="stacked">Email Address</IonLabel>
-          <IonInput onIonInput={(e: any) => setEmail(e.target.value)} clearOnEdit={true} placeholder="Enter email"></IonInput>
+          <IonInput
+            onIonInput={(e: any) => setEmail(e.target.value)}
+            clearOnEdit={true}
+            placeholder="Enter email"
+          ></IonInput>
         </div>
-        <div style={{ paddingTop: '20px', }}>
+        <div style={{ paddingTop: "20px" }}>
           <IonLabel position="stacked">Password</IonLabel>
-          <IonInput onIonInput={(e: any) => setPassword(e.target.value)} clearOnEdit={true} placeholder="Enter password"></IonInput>
+          <IonInput
+            onIonInput={(e: any) => setPassword(e.target.value)}
+            clearOnEdit={true}
+            placeholder="Enter password"
+          ></IonInput>
         </div>
-        <div style={{ paddingTop: '50px', }}>
-          <IonButton onClick={login} size="large" expand='block'>Log In</IonButton>
+        <div style={{ paddingTop: "50px" }}>
+          <IonButton onClick={login} size="large" expand="block">
+            Log In
+          </IonButton>
         </div>
-        <div style={{ paddingTop: '20px', textAlign: 'center' }}>
+        <div style={{ paddingTop: "20px", textAlign: "center" }}>
           <IonLabel>or</IonLabel>
         </div>
-        <div style={{ paddingTop: '20px', textAlign: 'center' }}>
-          <IonButton size="default" expand='block' onClick={() => { history.push('/register') }}>Sign Up</IonButton>
+        <div style={{ paddingTop: "20px", textAlign: "center" }}>
+          <IonButton
+            size="default"
+            expand="block"
+            onClick={() => {
+              history.push("/register");
+            }}
+          >
+            Sign Up
+          </IonButton>
         </div>
       </div>
-    </IonPage >
+    </IonPage>
   );
 };
 
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,47 +1,64 @@
-import { Computer } from '@bitcoin-computer/lib';
-import { IonApp, IonRouterOutlet, setupIonicReact, IonLabel, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-
+import { Computer } from "@bitcoin-computer/lib";
+import {
+  IonApp,
+  IonRouterOutlet,
+  setupIonicReact,
+  IonLabel,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 setupIonicReact();
 
-const Menu: React.FC = props => {
-    const location = useLocation<{ mnemonic: string, email: string}>();
-    const [computer] = useState(new Computer({
-        //mnemonic: location.state.mnemonic,
-        mnemonic: "travel upgrade inside soda birth essence junk merit never twenty system opinion",
-        chain: "LTC",
-        network: "testnet",
-        url: "https://node.bitcoincomputer.io",
-    }))
-    const [balance, setBalance] = useState(0)
-    const [amount, setAmount] = useState(0)
-    const [to, setTo] = useState("")
+const Menu: React.FC = (props) => {
+  const location = useLocation<{ mnemonic: string; email: string }>();
+  const [computer] = useState(
+    new Computer({
+      //mnemonic: location.state.mnemonic,
+      mnemonic:
+        "travel upgrade inside soda birth essence junk merit never twenty system opinion",
+      chain: "LTC",
+      network: "testnet",
+      url: "https://node.bitcoincomputer.io",
+    })
+  );
+  const [balance, setBalance] = useState(-1);
+  const [amount, setAmount] = useState(0);
+  const [to, setTo] = useState("");
 
-
-    useEffect(() => {
-        async function refresh() {
-            if (computer) setBalance(await computer.getBalance())
-        }
-        refresh()
-    }, [computer])
-    const Balance = () => {
-        if (balance === null) return <code>Loading...</code>
-        return <code>{balance / 1e8} {computer.getChain()} ({computer.getNetwork()})</code>
+  useEffect(() => {
+    async function refresh() {
+      if (computer) setBalance(await computer.getBalance());
     }
-
+    refresh();
+  }, [computer]);
+  const Balance = () => {
+    if (balance === -1) return <code>Loading...</code>;
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    {/* <IonTitle><Balance /></IonTitle> */}
-                    <IonTitle>Email: {location.state.email}</IonTitle>
-                    <IonLabel>Mnemonic: {location.state.mnemonic}</IonLabel>
-                </IonToolbar>
-            </IonHeader>
-        </IonPage>
+      <code>
+        {balance / 1e8} {computer.getChain()} ({computer.getNetwork()})
+      </code>
     );
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            <Balance />
+          </IonTitle>
+          <IonTitle>Email: {location.state.email}</IonTitle>
+          <IonLabel>Mnemonic: {location.state.mnemonic}</IonLabel>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
 };
 
 export default Menu;
