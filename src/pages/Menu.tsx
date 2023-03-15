31192d42f06ebb271b232c2e4715777b1f78683d
tommaso caputi <tommasocaputi85@gmail.com>
2023-03-15 19:06:27 +0100

    cleared files

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,24 +1,16 @@
 import {
-  IonApp,
-  IonRouterOutlet,
   setupIonicReact,
-  IonLabel,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
-  IonButton,
 } from "@ionic/react";
-import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 setupIonicReact();
 
 const Menu: React.FC = (props) => {
   const location = useLocation<{ email: string }>();
-  const [balance, setBalance] = useState(-1);
-  const [amount, setAmount] = useState(0);
-  const [to, setTo] = useState("");
 
   return (
     <IonPage>
