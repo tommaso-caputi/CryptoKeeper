dd2d459f5332e192b58575056e3ae77a8c1824fe
tommaso caputi <tommasocaputi85@gmail.com>
2023-03-15 20:48:51 +0100

    add balance show in menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -4,21 +4,35 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonText,
 } from "@ionic/react";
+import { useCallback, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 setupIonicReact();
 
-const Menu: React.FC = (props) => {
+const Menu: React.FC = () => {
   const location = useLocation<{ email: string, public_key: string, private_key: string, address: string, wif: string }>();
+  const [balance, setBalance] = useState(-1);
+
+  const fetchBalance = useCallback(async () => {
+    const data = await (await fetch('https://api.blockcypher.com/v1/btc/test3/addrs/' + location.state.address + '/balance')).json()
+    setBalance(data.balance / 100000000)
+    console.log('a')
+  }, [])
+
+  useEffect(() => {
+    fetchBalance()
+  }, [fetchBalance]);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Email: {location.state.email}</IonTitle>
-          <IonTitle>Address: {location.state.address}</IonTitle>
         </IonToolbar>
+        <IonText >Address: {location.state.address}</IonText><br />
+        <IonText>Balance: {balance}</IonText>
       </IonHeader>
     </IonPage>
   );
