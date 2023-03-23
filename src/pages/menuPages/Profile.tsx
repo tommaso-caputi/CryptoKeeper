9a6a7c9fc582dec7ee23227a8cc341a92f2cdfad
tommaso caputi <tommasocaputi85@gmail.com>
2023-03-23 19:49:43 +0100

    set full name and password change

--- src/pages/menuPages/Profile.tsx
+++ src/pages/menuPages/Profile.tsx
@@ -9,19 +9,51 @@ import {
     IonItem,
     IonLabel,
     IonInput,
+    useIonToast
 } from '@ionic/react';
+import sha256 from 'fast-sha256';
 import { useState } from 'react';
 import { useLocation } from "react-router-dom";
 
 
 function Profile() {
+    const [present] = useIonToast();
     const location = useLocation<{ email: string }>();
     const [fullname, setFullname] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState(location.state.email);
 
     const save = () => {
+        const passwordHash = Array.from(sha256(new TextEncoder().encode(password)))
+            .map(b => b.toString(16).padStart(2, '0'))
+            .join('')
         console.log(password, email, fullname);
+        fetch("http://cryptokeeper.altervista.org/APP/webhook.php", {
+            method: 'POST',
+            body: JSON.stringify({
+                "action": "update",
+                "email": location.state.email,
+                "password": passwordHash,
+                "fullname": fullname
+            })
+        })
+            .then(response => response.text())
+            .then(result => {
+                if (result === "Success") {
+                    present({
+                        message: 'Full name and password changed successfully',
+                        duration: 1500,
+                        position: 'bottom'
+                    });
+                } else {
+                    present({
+                        message: 'Something went wrong, try again',
+                        duration: 1500,
+                        position: 'bottom'
+                    });
+                }
+            })
+            .catch(error => console.log('error', error));
     }
 
     return (
@@ -47,14 +79,15 @@ function Profile() {
                     </IonItem>
                     <IonItem>
                         <IonLabel position="stacked">Email</IonLabel>
-                        <IonInput onIonInput={(e: any) => setEmail(e.target.value)} placeholder={location.state.email}></IonInput>
+                        <IonInput disabled={true} onIonInput={(e: any) => setEmail(e.target.value)} placeholder={location.state.email}></IonInput>
                     </IonItem>
                     <IonItem>
                         <IonLabel position="stacked">Password</IonLabel>
                         <IonInput onIonInput={(e: any) => setPassword(e.target.value)} placeholder="change password"></IonInput>
                     </IonItem>
                 </div>
-                <IonButton size="large" expand='block' onClick={save}>Save</IonButton>
+                <IonButton size="large" expand='block' onClick={save} >Save</IonButton>
+
             </IonContent>
         </>
     );
